eabb19772e55971e1f39b13d3f6477eb7466e777
ishwar <ishwardas051198@gmail.com>
2023-09-13 15:54:34 +0530

    after episode 3

--- App.js
+++ App.js
@@ -1,8 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+// let ele=React.createElement("h1",{},"hello from Namaste New");
 
+let jsxHeading = <h1 className="heading">Hello from JSX</h1>;
 
-let ele=React.createElement("h1",{},"hello from Namaste New");
+let parent = ReactDOM.createRoot(document.getElementById("root"));
+//functional component
 
-let parent=ReactDOM.createRoot(document.getElementById("root"));
+let Title = () => {
+  return <h1>this is title</h1>;
+};
+let ele=<h2>i am a react element</h2>
 
-parent.render(ele);
+let Bodytag = () => {
+  return (
+    <div>
+        {/* called component cpmposition */}
+      <Title />
+      <h2>this is inside body</h2>
+      {/* this is how we can put the react element inside the component  */}
+      {ele}
+    </div>
+  );
+};
+// parent.render(jsxHeading);
+
+parent.render(<Bodytag/>);
